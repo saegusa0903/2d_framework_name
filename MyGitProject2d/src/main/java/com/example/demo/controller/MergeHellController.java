055d108a54950687b77baa5fd1d4d5ad22c5e2e1
S0524 <S0524@21S0524>
2023-11-30 09:46:02 +0900

    aaa

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import java.util.Random;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		//kadaiA();
+		kadaiB();
+	}
+
+		static void kadaiB() {
+	//ここに課題を書く。メソッド名変えてね
+			Random rnd = new Random();
+			System.out.println(rnd.nextInt(99)+1);
+		}
+}
